5ddb36b1d20686281cb7e26d7e7e2eb2b23bdc42
Joel Clarke <joel.clarke273@gmail.com>
2019-07-16 16:50:05 +0100

    Implement loading, storing and usage of bookmark (most recent comment id)

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
+import os.path
 import praw
 
-OUT_STR = """
-Title: {}
-Text: {}
-Score: {}
-"""
+COMMENT_LIMIT = 100
+SAVE_FILE = "bookmark.txt"
+
+
+def load_bookmark():
+    with open(SAVE_FILE) as f:
+        return f.read()
+
+
+def save_bookmark(id):
+    with open(SAVE_FILE, "w") as f:
+        f.write(id)
 
 
 if __name__ == "__main__":
     reddit = praw.Reddit("bible-bot")
     subreddit = reddit.subreddit("dankchristianmemes")
 
-    for submission in subreddit.hot(limit=5):
-        print(OUT_STR.format(submission.title, submission.selftext, submission.score))
+    comments = subreddit.comments(limit=COMMENT_LIMIT)
+    bookmark = load_bookmark() if os.path.exists(SAVE_FILE) else None
+
+    for comment in comments:
+        # Stop if comment has been seen before
+        if bookmark == comment.id:
+            break
+
+        # Store most recent comment as bookmark
+        if comments.yielded == 1:
+            save_bookmark(comment.id)
+
+    
+    
