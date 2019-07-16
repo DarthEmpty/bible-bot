d333b84e4b34da20ce60d34f6d5dad4688a536bd
Joel Clarke <joel.clarke273@gmail.com>
2019-07-17 00:21:17 +0100

    Allow for passages to be extracted from text

--- bible_lookup.py
+++ bible_lookup.py
@@ -1,9 +1,15 @@
 import json
 from pprint import pprint
+import re
 import requests
 
 
 API_STRING = "https://getbible.net/json?text={}&ver={}"
+PASSAGE_PATTERN = r"\[\[(.*?)\]\]"
+
+
+def extract_passages(text):
+    return re.findall(PASSAGE_PATTERN, text)
 
 
 def lookup(ref, version="kjv"):
@@ -17,4 +23,7 @@ def lookup(ref, version="kjv"):
 
 
 if __name__ == "__main__":
-    pprint(lookup("John3:16-18"))
+    passages = extract_passages("I love what Jesus says in [[John3:16-18]] - especially when he says that bit about love in John3:16! Some of my other favourites are [[1 Corinthians 13]] and [[Romans 12:2]]")
+    
+    for passage in passages:        
+        pprint(lookup(passage))
