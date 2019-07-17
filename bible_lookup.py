61432029620bdeab1f65face8b8a8bee7814317a
Joel Clarke <joel.clarke273@gmail.com>
2019-07-17 01:01:20 +0100

    Allow passages to be paired with their references

--- bible_lookup.py
+++ bible_lookup.py
@@ -5,11 +5,11 @@ import requests
 
 
 API_STRING = "https://getbible.net/json?text={}&ver={}"
-PASSAGE_PATTERN = r"\[\[(.*?)\]\]"
+REFERENCE_PATTERN = r"\[\[(.*?)\]\]"
 
 
-def extract_passages(text):
-    return re.findall(PASSAGE_PATTERN, text)
+def extract_references(text):
+    return re.findall(REFERENCE_PATTERN, text)
 
 
 def lookup(ref, version="kjv"):
@@ -22,8 +22,10 @@ def lookup(ref, version="kjv"):
     return json.loads(stripped)
 
 
+def batch_lookup(refs):
+    return { ref: lookup(ref) for ref in refs }
+
+
 if __name__ == "__main__":
-    passages = extract_passages("I love what Jesus says in [[John3:16-18]] - especially when he says that bit about love in John3:16! Some of my other favourites are [[1 Corinthians 13]] and [[Romans 12:2]]")
-    
-    for passage in passages:        
-        pprint(lookup(passage))
+    passages = extract_references("I love what Jesus says in [[John3:16-18]] - especially when he says that bit about love in John3:16! Some of my other favourites are [[1 Corinthians 13]] and [[Romans 12:2]]")
+    pprint(batch_lookup(passages))
