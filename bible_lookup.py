b9b29d1ae4e64dcad1df5f6df3bcf8e61c1baba1
Joel Clarke <joel.clarke273@gmail.com>
2019-07-18 04:41:59 +0100

    Implement reply construction

--- bible_lookup.py
+++ bible_lookup.py
@@ -8,11 +8,11 @@ API_STRING = "https://getbible.net/json?text={}&ver={}"
 REFERENCE_PATTERN = r"\[\[(.*?)\]\]"
 
 
-def extract_references(text):
+def extract_references(text: str):
     return re.findall(REFERENCE_PATTERN, text)
 
 
-def lookup(ref, version="kjv"):
+def lookup(ref: str, version="kjv"):
     response = requests.get(API_STRING.format(ref, version))
     
     if response.status_code != 200:
@@ -22,10 +22,40 @@ def lookup(ref, version="kjv"):
     return json.loads(stripped)
 
 
-def batch_lookup(refs):
-    return { ref: lookup(ref) for ref in refs }
+def batch_lookup(refs: list):
+    return [ lookup(ref) for ref in refs ]
+
+
+def construct_reply(passage: dict):
+    verses = passage["chapter"]
+    verse_numbers = sorted(list(verses.keys()), key=lambda k: int(k))
+
+    reply = "{} {} ({})\n\n".format(passage["book_name"], passage["chapter_nr"], passage["version"])
+    for num in verse_numbers:
+        reply += "{}: {}".format(num, verses[num]["verse"])
+
+    return reply
+
+
+def construct_replies(passages: list):
+    replies = []
+
+    for passage in passages:
+        if "book" in passage:
+            for section in passage["book"]:
+                section["version"] = passage["version"]
+
+            replies.extend(construct_replies(passage["book"]))
+            
+        else:
+            replies.append(construct_reply(passage))
+    
+    return replies
+
 
 
 if __name__ == "__main__":
     passages = extract_references("I love what Jesus says in [[John3:16-18]] - especially when he says that bit about love in John3:16! Some of my other favourites are [[1 Corinthians 13]] and [[Romans 12:2]]")
-    pprint(batch_lookup(passages))
+    res = batch_lookup(passages)
+    replies = construct_replies(res)
+    pprint(replies)
